4aab36e5a1a796745d7e474056d9350d18d43b2f
Christian Jay B. Dimasayao <89844193+cjbdimasayao@users.noreply.github.com>
2023-03-18 18:37:16 +0800

    Add files via upload

---
+++ ordermodule_php/order_placed.php
@@ -0,0 +1,15 @@
+<!-- shop.php -->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Shop</title>
+</head>
+<body>
+	<h1>ORDER PLACED!</h1>
+	<form action="order_list.php" method="post">
+	<input type="submit" value="View Order">
+	</form>
+
+</body>
+</html>
